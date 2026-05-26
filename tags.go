824e4ebad7d531fe2136a5cb53677d38e19487bb
Koki Matsumoto <git@rokuosan.dev>
2026-05-27 01:46:34 +0900

    Add TOML tag option parsing

--- tags.go
+++ tags.go
@@ -5,6 +5,10 @@ import (
 	"strings"
 )
 
+// tagOptions is the string following a comma in a struct field's "toml"
+// tag, or the empty string. It does not include the leading comma.
+type tagOptions string
+
 func cachedFields(t reflect.Type) map[string]int {
 	fields := map[string]int{}
 	for i := 0; i < t.NumField(); i++ {
@@ -22,11 +26,32 @@ func cachedFields(t reflect.Type) map[string]int {
 	return fields
 }
 
-func tagName(f reflect.StructField) string {
-	name := f.Tag.Get("toml")
-	if comma := strings.IndexByte(name, ','); comma >= 0 {
-		name = name[:comma]
+// parseTag splits a struct field's toml tag into its name and comma-separated
+// options.
+func parseTag(tag string) (string, tagOptions) {
+	name, opt, _ := strings.Cut(tag, ",")
+	return name, tagOptions(opt)
+}
+
+// Contains reports whether a comma-separated list of options contains
+// optionName.
+func (o tagOptions) Contains(optionName string) bool {
+	if len(o) == 0 {
+		return false
 	}
+	s := string(o)
+	for s != "" {
+		var name string
+		name, s, _ = strings.Cut(s, ",")
+		if name == optionName {
+			return true
+		}
+	}
+	return false
+}
+
+func tagName(f reflect.StructField) string {
+	name, _ := parseTag(f.Tag.Get("toml"))
 	if name == "" {
 		name = f.Name
 	}
