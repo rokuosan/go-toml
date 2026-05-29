ac33ec8ebaaa9b97f51fff5e4f50d0bd1b8878a7
Koki Matsumoto <git@rokuosan.dev>
2026-05-29 11:41:30 +0900

    Support setting document scalar values

--- document_parser.go
+++ document_parser.go
@@ -7,25 +7,26 @@ func (p *parser) parseSyntaxDocument() (*SyntaxDocument, error) {
 		return nil, p.errAt(0, "input is not valid UTF-8")
 	}
 	doc := &SyntaxDocument{source: p.input, doc: p.doc}
+	var currentPath []string
 	for !p.eof() {
 		lineStart := p.pos
 		p.skipWS()
 		if p.eof() {
 			if p.pos > lineStart {
-				doc.appendNode(SyntaxBlankLine, lineStart, p.pos, 0, 0, nil)
+				doc.appendNode(SyntaxBlankLine, lineStart, p.pos, 0, 0, nil, nil)
 			}
 			break
 		}
 		switch p.peek() {
 		case '\n':
 			p.pos++
-			doc.appendNode(SyntaxBlankLine, lineStart, p.pos, 0, 0, nil)
+			doc.appendNode(SyntaxBlankLine, lineStart, p.pos, 0, 0, nil, nil)
 			continue
 		case '\r':
 			if !p.consume("\r\n") {
 				return nil, p.err("bare carriage return")
 			}
-			doc.appendNode(SyntaxBlankLine, lineStart, p.pos, 0, 0, nil)
+			doc.appendNode(SyntaxBlankLine, lineStart, p.pos, 0, 0, nil, nil)
 			continue
 		case '#':
 			if err := p.skipComment(); err != nil {
@@ -34,7 +35,7 @@ func (p *parser) parseSyntaxDocument() (*SyntaxDocument, error) {
 			if err := p.finishSyntaxLine(); err != nil {
 				return nil, err
 			}
-			doc.appendNode(SyntaxComment, lineStart, p.pos, 0, 0, nil)
+			doc.appendNode(SyntaxComment, lineStart, p.pos, 0, 0, nil, nil)
 			continue
 		case '[':
 			key, array, keyStart, keyEnd, err := p.parseSyntaxTable()
@@ -48,23 +49,33 @@ func (p *parser) parseSyntaxDocument() (*SyntaxDocument, error) {
 			if array {
 				kind = SyntaxArrayTable
 			}
-			doc.appendNode(kind, lineStart, p.pos, keyStart, keyEnd, key)
+			currentPath = append([]string(nil), key...)
+			doc.appendNode(kind, lineStart, p.pos, keyStart, keyEnd, key, currentPath)
 		default:
 			keyStart := p.pos
-			key, keyEnd, valueStart, valueEnd, err := p.parseSyntaxKeyValue()
+			kv, err := p.parseSyntaxKeyValue()
 			if err != nil {
 				return nil, err
 			}
 			if err := p.finishSyntaxLine(); err != nil {
 				return nil, err
 			}
-			doc.appendKeyValueNode(lineStart, p.pos, keyStart, keyEnd, valueStart, valueEnd, key)
+			path := append(append([]string(nil), currentPath...), kv.key...)
+			doc.appendKeyValueNode(lineStart, p.pos, keyStart, kv.keyEnd, kv.valueStart, kv.valueEnd, kv.key, path, kv.value)
 		}
 	}
 	return doc, nil
 }
 
-func (d *SyntaxDocument) appendNode(kind SyntaxKind, start, end, keyStart, keyEnd int, key []string) {
+type syntaxKeyValue struct {
+	key        []string
+	value      any
+	keyEnd     int
+	valueStart int
+	valueEnd   int
+}
+
+func (d *SyntaxDocument) appendNode(kind SyntaxKind, start, end, keyStart, keyEnd int, key, path []string) {
 	node := SyntaxNode{
 		Kind:     kind,
 		Start:    start,
@@ -72,13 +83,14 @@ func (d *SyntaxDocument) appendNode(kind SyntaxKind, start, end, keyStart, keyEn
 		KeyStart: keyStart,
 		KeyEnd:   keyEnd,
 		Key:      append([]string(nil), key...),
+		Path:     append([]string(nil), path...),
 		Raw:      d.source[start:end],
 		RawKey:   d.source[keyStart:keyEnd],
 	}
 	d.nodes = append(d.nodes, node)
 }
 
-func (d *SyntaxDocument) appendKeyValueNode(start, end, keyStart, keyEnd, valueStart, valueEnd int, key []string) {
+func (d *SyntaxDocument) appendKeyValueNode(start, end, keyStart, keyEnd, valueStart, valueEnd int, key, path []string, value any) {
 	node := SyntaxNode{
 		Kind:       SyntaxKeyValue,
 		Start:      start,
@@ -88,6 +100,8 @@ func (d *SyntaxDocument) appendKeyValueNode(start, end, keyStart, keyEnd, valueS
 		ValueStart: valueStart,
 		ValueEnd:   valueEnd,
 		Key:        append([]string(nil), key...),
+		Path:       append([]string(nil), path...),
+		Value:      value,
 		Raw:        d.source[start:end],
 		RawKey:     d.source[keyStart:keyEnd],
 		RawValue:   d.source[valueStart:valueEnd],
@@ -154,25 +168,31 @@ func (p *parser) parseSyntaxTable() ([]string, bool, int, int, error) {
 	return key, false, keyStart, keyEnd, nil
 }
 
-func (p *parser) parseSyntaxKeyValue() ([]string, int, int, int, error) {
+func (p *parser) parseSyntaxKeyValue() (syntaxKeyValue, error) {
 	key, err := p.parseKey()
 	if err != nil {
-		return nil, 0, 0, 0, err
+		return syntaxKeyValue{}, err
 	}
 	keyEnd := p.pos
 	p.skipWS()
 	if !p.consume("=") {
-		return nil, 0, 0, 0, p.err("expected =")
+		return syntaxKeyValue{}, p.err("expected =")
 	}
 	p.skipWS()
 	valueStart := p.pos
 	val, err := p.parseValue()
 	if err != nil {
-		return nil, 0, 0, 0, err
+		return syntaxKeyValue{}, err
 	}
 	valueEnd := p.pos
 	if err := p.setValue(p.current, key, val); err != nil {
-		return nil, 0, 0, 0, err
+		return syntaxKeyValue{}, err
 	}
-	return key, keyEnd, valueStart, valueEnd, nil
+	return syntaxKeyValue{
+		key:        key,
+		value:      val,
+		keyEnd:     keyEnd,
+		valueStart: valueStart,
+		valueEnd:   valueEnd,
+	}, nil
 }
